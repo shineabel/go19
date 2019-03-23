5bce0162e9b8d75695c451b554918c19d6957392
xiaoxianming <xianming@bianjie.ai>
2019-03-23 21:14:10 +0800

    mutex

--- base/r2.go
+++ base/r2.go
@@ -4,32 +4,37 @@ import (
 	"sync"
 	"fmt"
 	"sync/atomic"
-	"runtime"
 	"time"
+	"runtime"
 )
 
 var (
 	counter int64
 	wg sync.WaitGroup
 	shoudown int64
+	m sync.Mutex
 )
 
 func main() {
 	wg.Add(2)
-	//go inc(1)
-	//go inc(2)
-	//
-	//fmt.Println("wait routine")
-	//wg.Wait()
-	//fmt.Printf("end", counter)
-	go doWork("a")
-	go doWork("b")
+	go inc(1)
+	go inc(2)
 
-	time.Sleep(1 * time.Second)
-	fmt.Printf("shutdown now")
-	atomic.StoreInt64(&shoudown,1)
-
+	fmt.Println("wait routine")
 	wg.Wait()
+	fmt.Printf("end %d", counter)
+
+
+
+
+	//go doWork("a")
+	//go doWork("b")
+	//
+	//time.Sleep(1 * time.Second)
+	//fmt.Printf("shutdown now")
+	//atomic.StoreInt64(&shoudown,1)
+	//
+	//wg.Wait()
 
 }
 
@@ -38,12 +43,17 @@ func inc( c int)  {
 	defer  wg.Done()
 
 	for count := 0 ; count < 2; count++ {
-		//value := counter
+
+		m.Lock()
+		{
+			value := counter
+			runtime.Gosched()
+			value++
+			counter = value
+		}
+		m.Unlock()
+		//atomic.AddInt64(&counter,1)
 		//runtime.Gosched()
-		//value++
-		//counter = value
-		atomic.AddInt64(&counter,1)
-		runtime.Gosched()
 	}
 }
 
