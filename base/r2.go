1ee4621c73290d7c7f1f0252e59c698f807831aa
xiaoxianming <xianming@bianjie.ai>
2019-03-23 21:08:16 +0800

    atomic

--- base/r2.go
+++ base/r2.go
@@ -5,21 +5,31 @@ import (
 	"fmt"
 	"sync/atomic"
 	"runtime"
+	"time"
 )
 
 var (
 	counter int64
 	wg sync.WaitGroup
+	shoudown int64
 )
 
 func main() {
 	wg.Add(2)
-	go inc(1)
-	go inc(2)
+	//go inc(1)
+	//go inc(2)
+	//
+	//fmt.Println("wait routine")
+	//wg.Wait()
+	//fmt.Printf("end", counter)
+	go doWork("a")
+	go doWork("b")
 
-	fmt.Println("wait routine")
+	time.Sleep(1 * time.Second)
+	fmt.Printf("shutdown now")
+	atomic.StoreInt64(&shoudown,1)
+
 	wg.Wait()
-	fmt.Printf("end", counter)
 
 }
 
@@ -34,5 +44,19 @@ func inc( c int)  {
 		//counter = value
 		atomic.AddInt64(&counter,1)
 		runtime.Gosched()
+	}
+}
+
+func doWork(name string)  {
+	defer wg.Done()
+	for {
+
+		fmt.Printf("do work ,name:%s \n", name)
+		time.Sleep(250 * time.Millisecond)
+
+		if atomic.LoadInt64(&shoudown) == 1 {
+			fmt.Printf("shuwdown work: %s \n", name)
+			break
+		}
 	}
 }
