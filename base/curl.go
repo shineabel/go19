354a7b4b968643d73992ca3bedd7e1d941139d7b
xiaoxianming <xianming@bianjie.ai>
2019-03-23 19:42:25 +0800

    201903-json

--- base/curl.go
+++ base/curl.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"bytes"
+	"os"
 	"fmt"
 	"io"
-	"os"
+	"net/http"
+	"log"
 )
 
 //func init() {
@@ -15,22 +16,28 @@ import (
 //	}
 //}
 func main() {
-	//resp, err := http.Get(os.Args[1])
-	//if(err != nil){
-	//	fmt.Println("error:",err)
-	//	return
-	//}
-	//
-	//io.Copy(os.Stdout,resp.Body)
-	//
-	//if err = resp.Body.Close(); err != nil {
-	//	fmt.Println("close error:",err)
-	//}
+	resp, err := http.Get(os.Args[1])
+	if(err != nil){
+		fmt.Println("error:",err)
+		return
+	}
+	f, err2 := os.Create(os.Args[2])
+	if(err2 != nil){
+		fmt.Println(err2)
+	}
 
-	var b bytes.Buffer
-	b.Write([]byte("hello"))
+	dest := io.MultiWriter(os.Stdout,f)
+	io.Copy(dest,resp.Body)
+	if err3 := resp.Body.Close();err3 != nil {
+		log.Println(err3)
+	}
 
-	fmt.Fprint(&b,", world")
-	io.Copy(os.Stdout,&b)
+
+
+	//var b bytes.Buffer
+	//b.Write([]byte("hello"))
+	//
+	//fmt.Fprint(&b,", world")
+	//io.Copy(os.Stdout,&b)
 
 }
--- base/json.go
+++ base/json.go
@@ -1,52 +1,37 @@
 package main
 
 import (
-	"net/http"
 	"encoding/json"
 	"fmt"
 )
 
-type (
+type Contact struct {
+	Name string `json:"name"`
+	Title string `json:"title"`
+	Detail struct{
+		Home string `json:"home"`
+		Phone string `json:"phone"`
+	}  `json:"detail"`
 
+}
 
+var str = `{
+"name":"shine",
+"title":"manager",
+"detail":{
+  "home":"shanghai",
+  "phone":"13888888888"
+}
 
-	gResult struct {
-		Title string `json:"title"`
-		TitleNoFormating string `json:"titleNoFormating"`
-		Content string `json:"content"`
-		cacheURL string `json:"cacheUrl"`
-		visibleURL string `json:"visibleUrl"`
-		URL string `json:"url"`
-		GSearchResultClass string `json:"GsearchResultClass"`
-		unescapedURL string `json:"unescapedUrl"`
-
-	}
-
-	gResponse struct {
-
-		ResponseData struct{
-			Results []gResult `json:"results"`
-
-		} `json:"responseData"`
-	}
-)
+}`
 
 func main() {
 
-	uri := "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=8&q=golang"
-	resp, err := http.Get(uri)
-	if(err != nil){
-		fmt.Println("query failed:",err)
-	}
-	defer resp.Body.Close()
-	var gr gResponse
+var c Contact
 
-
-	err = json.NewDecoder(resp.Body).Decode(&gr)
-	if(err != nil){
-		fmt.Println("error")
-		return
-	}
-	fmt.Println(gr)
-
+err := json.Unmarshal([]byte(str),&c)
+if(err != nil){
+	fmt.Println(err)
+}
+fmt.Println(c)
 }
