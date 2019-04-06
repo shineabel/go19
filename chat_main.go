71d33f8233be91b8fc701ec1335a6d1587dcdb1c
xiaoxianming <xianming@bianjie.ai>
2019-04-06 16:16:46 +0800

    chat demo

---
+++ chat_main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go19/chat"
+)
+
+func main() {
+	fmt.Printf("start chat room")
+
+	server := &chat.ChatServer{
+
+		Bind2:":12345",
+		Rooms: make(map[string]*chat.Room),
+	}
+
+	server.ListenAndServe()
+}
